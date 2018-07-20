9ac10f0b4f708bcb9cb569a30dedb49c87df7b69
Aleksandr Zhuikov <aleksandr@transferwise.com>
2018-07-20 09:40:33 +0300

    switched to mysensors

--- temperature/temperature.ino
+++ temperature/temperature.ino
@@ -1,47 +1,77 @@
-#include <RH_ASK.h>
-#include <SPI.h>
-//#include "LowPower.h"
 
-RH_ASK driver = RH_ASK(2000, 11, 9, 10, false);
+#define MY_DEBUG
+#define MY_BAUD_RATE 38400
+#define MY_RADIO_NRF24
 
-int ThermistorPin = 0;
-int ledPin = 13;      // select the pin for the LED
+#define CHILD_ID_TEMP_1 0
+#define CHILD_ID_TEMP_2 1
 
-int Vo;
+//#include <MySensors.h> 
+
+//MyMessage msgTemp1(CHILD_ID_TEMP_1, V_TEMP);
+//MyMessage msgTemp2(CHILD_ID_TEMP_2, V_TEMP);
+
+#define THERMISTOR_PIN_1 A6
+#define THERMISTOR_PIN_2 A1
+
 float R1 = 10000;
-float logR2, R2, T;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
-int intTemperature;
-char address = 'A';
+float lastTemperature1 = 0.0;
+float lastTemperature2 = 0.0;
 
+void presentation()  
+{ 
+//  // Send the sketch version information to the gateway
+//  sendSketchInfo("temperature", "1.0");
+//
+//  // Register all sensors to gw (they will be created as child devices)
+//  present(CHILD_ID_TEMP_1, S_TEMP);
+//  present(CHILD_ID_TEMP_2, S_TEMP);
+}
+ 
 void setup() {
-//  pinMode(ledPin, OUTPUT);     
-  Serial.begin(9600); // Debugging only
-  if (!driver.init())
-     Serial.println("init failed");
+  pinMode(THERMISTOR_PIN_1, INPUT);
+  pinMode(THERMISTOR_PIN_2, INPUT);
+  Serial.begin(38400); // Debugging only
 } 
 
 void loop() {
   
-  Vo = analogRead(ThermistorPin);
-  R2 = R1 * (1023.0 / (float)Vo - 1.0); 
-  logR2 = log(R2);
-  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
-  T = T - 273.15;
-//  T = (byte) (T* 10);
-  intTemperature = (int) (T * 100);
+  float T = readTemperature(THERMISTOR_PIN_1);
 
-//    const char *msg = "Hello World!";
-//    driver.send((uint8_t *)msg, strlen(msg));
+  #ifdef MY_DEBUG
+//    Serial.println(T);
+  #endif
 
-  uint8_t data[] = {(uint8_t) address, (uint8_t) highByte(intTemperature), (uint8_t) lowByte(intTemperature)};
-  driver.send(data, (uint8_t) 3);
-  driver.waitPacketSent();
+  if (lastTemperature1 != T) {
+//    send(msgTemp1.set(T, 1));
+    lastTemperature1 = T;
+  }
 
- // Serial.print(intTemperature);
+//  T = readTemperature(THERMISTOR_PIN_2);
+//
+//  #ifdef MY_DEBUG
+//    Serial.println(T);
+//  #endif
+//
+//  if (lastTemperature2 != T) {
+//    send(msgTemp2.set(T, 1));
+//    lastTemperature2 = T;
+//  }
 
-//  LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
-  delay(1000);
+    delay(1000);
+//  sleep(1000);
 }
 
+
+float readTemperature(int pin) {
+  int Vo = analogRead(pin);
+  #ifdef MY_DEBUG
+    Serial.println(Vo);
+  #endif
+  float R2 = R1 * (1023.0 / (float)Vo - 1.0); 
+  float logR2 = log(R2);
+  float t = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  return round((t - 273.15) * 2) / 2;
+}
 
